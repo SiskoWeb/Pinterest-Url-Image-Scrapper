5147fc8f81917e775ca1404c8dd5b5fd03c561de
SiskoWeb <sisko4dev@gmail.com>
2023-09-28 00:13:28 +0100

    change url

--- src/app/api/scrapping/route.ts
+++ src/app/api/scrapping/route.ts
@@ -4,9 +4,16 @@ const { scrollPageToBottom } = require("puppeteer-autoscroll-down");
 
 import { JSDOM } from "jsdom";
 
-export async function GET(request: Request) {
-  const url =
-    "https://www.pinterest.com/search/pins/?rs=ac&len=2&q=one%20piece";
+export async function POST(request: Request) {
+  const { keyword: userSearch } = await request.json();
+
+  if (!userSearch) {
+    return NextResponse.json(
+      { error: "Search parameter not provided" },
+      { status: 400 }
+    );
+  }
+  const url = `https://www.pinterest.com/search/pins/?rs=ac&len=2&q=${userSearch}`;
   let browser;
   try {
     browser = await puppeteer.launch({ headless: "new" });
@@ -27,13 +34,21 @@ export async function GET(request: Request) {
     const dom = new JSDOM(data, { includeNodeLocations: true }); //load the html content
 
     const document: any = dom.window.document;
-    const imageUrl: any = await document.querySelectorAll(
+
+    const imageUrl: any | null = await document.querySelectorAll(
       "img.hCL.kVc.L4E.MIw"
     );
-    console.log(imageUrl.length);
+
+    const urls = await imageUrl.forEach((item: any) => {
+      console.log(item.getAttribute("src").replace("236x", "originals"));
+    });
+
     const allUrls = await imageUrl[13].getAttribute("src");
+
     const urlImg = await allUrls.replace("236x", "originals");
+
     await page.screenshot({ path: `./second.jpg` });
+
     return NextResponse.json({ data: urlImg }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 200 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,59 @@
 "use client";
-
-import { FormEvent } from "react";
+import { headers } from "next/dist/client/components/headers";
+import { FormEvent, useState } from "react";
+import Loader from "./components/Loader";
 export default function Home() {
+  const [keyword, setKeyword] = useState<string>("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("start");
+    setIsLoading(true);
 
-    const res = await fetch("api/scrapping");
+    if (keyword === "") {
+      setIsLoading(false);
+      
+      return;
+    }
+    const res = await fetch("api/scrapping", {
+      method: "POST",
+      body: JSON.stringify({ keyword }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     const data = await res.json();
 
     console.log(res);
     console.log(data);
+    setIsLoading(false);
+    setKeyword("");
     console.log("done");
   };
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+    <div className="  relative bg-gray-900 min-h-screen flex items-center justify-center">
       <main className=" flex flex-col min-h-screen items-center justify-center gap-20">
         <section>
           <h1 className="text-white font-bold text-center md:text-8xl text-6xl">
-            Start tracking <br></br>your apps{" "}
+            Start tracking
           </h1>
         </section>
+
+        {isLoading ? <Loader /> : null}
+
         <form
           onSubmit={(e) => handleSubmit(e)}
           className="flex flex-col gap-3  "
         >
-          <input className="rounded-md p-3" type="text" placeholder="Email" />
+          <input
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            className="rounded-md p-3"
+            type="text"
+            placeholder="name Wallpaper"
+          />
 
           <button className="bg-[#2a66f9] text-white rounded-md font-bold cursor-pointer px-6 py-2">
             Login
